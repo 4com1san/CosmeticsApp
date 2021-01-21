daac1e801385e7bed063dc71ad04ad8ee249ef05
jun-k0 <wnsrlsladlek@naver.com>
2021-01-21 23:07:32 +0900

    회원 속성 추가

--- backend/login/src/main/java/com1san/cosmeticsApp/domain/Member.java
+++ backend/login/src/main/java/com1san/cosmeticsApp/domain/Member.java
@@ -6,16 +6,15 @@ import lombok.Setter;
 import javax.persistence.*;
 
 @Entity
-@Getter @Setter
+@Getter
+@Setter
 public class Member {
-    @Id @GeneratedValue
-    @Column(name="member_id")
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
-
     private String name;
-
     private Long password;
-
     private String nickname;
 
     @Enumerated(EnumType.STRING)
@@ -23,6 +22,23 @@ public class Member {
 
     @Enumerated(EnumType.STRING)
     private SensitiveStatus sensitive_status;
+    //피부고민 엔티티 나눠야하나?
+    private boolean blackhead;
+    private boolean oily;
+    private boolean keratin;
+    private boolean pimple;
+    private boolean dry;
+    private boolean glow;
+    private boolean flexibility;
+    private boolean skintone;
+    private boolean wrinkle;
+    //개인특성
+    private Long Sleeping_Hours;
+    private Long Wash_Temperature;
+    private Long Wash_Num;
+    private String Stress;
+    private String Collyrium;
+    private String Food;
 }
 /*
 Name: '' 로그인id
@@ -31,6 +47,13 @@ PWT: '' 비번확인
 EMAIL: '' 이멜(보류)
 NickNAME: '' 닉네임
 list:"지성" 피부타입
-skinBool0,1,2,...:"" boolean으로 피부고민
 api 명세
+blackhead, oily, keratin, pimple, dry, glow, flexibility, skintone, wrinkle 여기까지가 boolean으로
+Sleeping_Hours
+    Wash_Temperature
+    Wash_Num
+    Stress
+    Collyrium
+    Food 이게 그냥 스트링 값으로 보낼듯
+
  */
